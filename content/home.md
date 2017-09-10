a3e6c7cc87cc7ec5653de7c049a748c796d8f87d
Eliane Faveron Maciel <elinefav94@gmail.com>
2017-09-10 16:57:56 -0300

    Adicionado pagina sobre o pyladies

--- content/home.md
+++ content/home.md
@@ -1,5 +1,5 @@
 Title: Pyladies Caxias
 Date: 2017-09-10 15:10
-Category: Review
+Category: Home
 
 Pyladies Caxias do Sul
--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'America/Sao_Paulo'
 
 DEFAULT_LANG = u'pt'
 
+DISPLAY_PAGES_ON_MENU = True
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
